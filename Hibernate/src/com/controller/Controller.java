64fd346aebf2229d20758a064f6ec31816ac8fe3
Maria <Maria@192.168.0.248>
2017-11-12 13:48:28 +0100

    JSP Form for input: FName LName SName Street1 Street2
    will save in two DB Tables:
    Student(FName, LName, MName)
    Adress(Street)

--- Hibernate/src/com/controller/Controller.java
+++ Hibernate/src/com/controller/Controller.java
@@ -1,27 +1,81 @@
 package com.controller;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
+import com.model.Address;
 import com.model.Student;
+import com.model.StudentName;
 
-public class Controller {
+import antlr.collections.Enumerator;
 
-	public static void main(String[] args) {
-		
-		Student student = new Student();
-		
-		student.setName("Vadym");
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+ 
 
-		Configuration con = new Configuration().addAnnotatedClass(Student.class);
-		con.configure();
-		SessionFactory sf = con.buildSessionFactory();
-		Session session = sf.openSession();
-		Transaction tx = session.beginTransaction();
-		session.save(student);
-		tx.commit();
-	}
+public class Controller{
+
+//@Override
+//protected void doPost(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+//		System.out.println("I'm here");
+//		
+//		Student student = new Student();
+//		student.setId(1);
+//		StudentName sname = new StudentName();
+//		sname.setFName(request.getParameter("sname"));
+//		sname.setLName("Yershon");
+//		sname.setMName("Grigorjevich");
+//		student.setSname(sname);
+//		
+//		Address address = new Address();
+//		address.setId(101);
+//		address.setStreet("Görlitzer Str.");
+//		
+//		Address address1 = new Address();
+//		address1.setId(102);
+//		address1.setStreet("Freiburgeroberfeld Str.");
+//		
+//		List<Address> L_address = new ArrayList<>();
+//		student.setAddress(L_address);
+//		student.getAddress().add(address);
+//		student.getAddress().add(address1);
+//		
+//		Configuration con = new Configuration().addAnnotatedClass(Student.class).addAnnotatedClass(Address.class);
+//		con.configure();
+//		SessionFactory sf = con.buildSessionFactory();
+//		Session session = sf.openSession();
+//		Transaction tx = session.beginTransaction();
+//		
+//		session.save(student);
+//		session.save(address);
+//		session.save(address1);
+//		
+//		Student student_fetch = new Student();
+//		Address address_fetch = new Address();
+//		
+//		student_fetch = (Student)session.get(Student.class, 1);
+//		System.out.println("##### The student with ID = 1:   " + student_fetch.toString());
+//		
+//		address_fetch = (Address)session.get(Address.class, 101);
+//		System.out.println("***** The Address with ID = 101:   " + address_fetch.toString());
+//		tx.commit();
+//		session.close();
+//	}
+//    
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//    	// TODO Auto-generated method stub
+//    	super.doGet(req, resp);
+//    }
 
 }
--- Hibernate/src/com/model/Student.java
+++ Hibernate/src/com/model/Student.java
@@ -1,9 +1,14 @@
 package com.model;
 
+import java.util.List;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 import org.hibernate.annotations.Generated;
 
@@ -11,10 +16,29 @@ import org.hibernate.annotations.Generated;
 public class Student {
 	
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+//	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private int id;
+	@Column(name="Name_Column")
+	private StudentName sname;
+	@OneToMany(mappedBy = "student")
+	private List<Address> address;
 	
-	private String Name;
+
+	public List<Address> getAddress() {
+		return address;
+	}
+
+	public void setAddress(List<Address> address) {
+		this.address = address;
+	}
+
+	public StudentName getSname() {
+		return sname;
+	}
+
+	public void setSname(StudentName sname) {
+		this.sname = sname;
+	}
 
 	public int getId() {
 		return id;
@@ -24,12 +48,17 @@ public class Student {
 		this.id = id;
 	}
 	
-	public String getName() {
-		return Name;
-	}
-	
-	public void setName(String name) {
-		Name = name;
+//	public String getName() {
+//		return Name;
+//	}
+//	
+//	public void setName(String name) {
+//		Name = name;
+//	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", Name=" + sname.getFName()+","+ sname.getMName() +","+ sname.getLName() + "]";
 	}
 
 }
